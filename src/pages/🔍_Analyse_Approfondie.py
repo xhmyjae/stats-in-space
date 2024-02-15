0f79ea1f7aace21a31d0be7167a9507fa57fd6c1
Ayfri <pierre.ayfri@gmail.com>
2024-02-15 21:17:31 +0100

    feat(website): Add more final version of website with multiple pages, to be filled.

---
+++ src/pages/🔍_Analyse_Approfondie.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.title("L'évolution de l'open source sur GitHub")
+
+st.header("Analyse avancée des données")
+
+st.subheader("- Évolution du nombre de dépôts par langage")
+
+st.subheader("- Corrélations entre langages et topics")
+
+st.subheader("- Popularité relative des langages sur 1 an et 5 ans, par topic")
+
+st.subheader("- Corrélation entre taille, licences, langages, topics")
